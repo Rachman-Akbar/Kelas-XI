198a3521bf9238f7cadb336e08dd822497e8abed
Mochammad Rachman Akbar Fahlevy <akbarfahlevy39@gmail.com>
2026-04-06 13:08:36 +0700

    Delete UKOMP directory

--- UKOMP/1. Layout/Business/app/src/main/java/com/komputerkit/business/models/CartItem.kt
+++
@@ -1,9 +0,0 @@
-package com.komputerkit.business.models
-
-data class CartItem(
-    val id: String,
-    val name: String,
-    val price: Double,
-    val quantity: Int,
-    val image: String = ""
-)
